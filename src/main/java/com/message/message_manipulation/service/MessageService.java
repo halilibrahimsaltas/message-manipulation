b61eed7a14e3ab3ebe91ad2d2d01dbf59e571563
Halil İbrahim <halil.ali.gs.1430@gmail.com>
2025-02-10 17:20:16 +0300

    Service

--- src/main/java/com/message/message_manipulation/service/MessageService.java
+++ src/main/java/com/message/message_manipulation/service/MessageService.java
@@ -5,6 +5,8 @@ import com.message.message_manipulation.repository.MessageRepository;
 import lombok.RequiredArgsConstructor;
 import com.message.message_manipulation.model.Message;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +19,40 @@ public class MessageService {
         return messageRepository.findAll();
     }
 
+
+    public Message saveMessage(String content) {
+        Message message = new Message();
+        message.setContent(content);
+        message.setConvertedText(convertLinks(content));
+        
+        return messageRepository.save(message);
+    }
+
+    public Message getMessageById(Long id) {
+        return messageRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Message not found"));
+    }
+
+
+    private String convertLinks(String text) {
+        // URL pattern'i
+        String urlPattern = "https?://[\\w-]+(\\.[\\w-]+)+([\\w.,@?^=%&:/~+#-]*[\\w@?^=%&/~+#-])?";
+        Pattern pattern = Pattern.compile(urlPattern);
+        Matcher matcher = pattern.matcher(text);
+        
+        StringBuffer result = new StringBuffer();
+        while (matcher.find()) {
+            String url = matcher.group();
+            String modifiedUrl = url.replaceAll("ref=\\w+", "ref=MY_REF");
+            matcher.appendReplacement(result, Matcher.quoteReplacement(modifiedUrl));
+        }
+        matcher.appendTail(result);
+        
+        return result.toString();
+    }
+    
     
 
+
 }
+
